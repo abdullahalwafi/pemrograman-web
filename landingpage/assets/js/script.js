0b5767f580ef049be38e5ef8b9528fc04245f425
abdullahalwafi <abdullahalwafi@gmial.com>
2022-11-22 08:26:35 +0700

    First Landing Page

---
+++ landingpage/assets/js/script.js
@@ -0,0 +1,24 @@
+const trigger = document.querySelectorAll('nav li');
+    trigger.forEach((menu) => menu.addEventListener('click', toggle));
+    
+    function toggle() {
+        trigger.forEach((item) => item != this ? item.classList.remove('active') : null);
+        if (this.classList != 'active') {
+            this.classList.toggle('active')
+        }
+    }
+const mediaQuery = window.matchMedia('(min-width: 601px)')
+if (mediaQuery.matches) {
+    $(document).ready(function () {
+        $(window).scroll(function () {
+            $(window).height()
+            if ($(window).scrollTop() > 1) {
+                $("#navbar").css({ "background-color": "#ACD1FE" });
+            }
+            else {
+                $("#navbar").css({ "background-color": "transparent" });
+            }
+
+        })
+    })
+}
