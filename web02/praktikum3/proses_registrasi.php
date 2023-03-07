54378443bc2c69a27186f0a7947a51888e821af6
abdullahalwafi <abdullahalwafi16@gmail.com>
2023-03-07 15:34:03 +0700

    Upload Proses_register.php

---
+++ web02/praktikum3/proses_registrasi.php
@@ -0,0 +1,17 @@
+<?php
+$domisili = ['jakarta', 'bogor', 'depok', 'tanggerang', 'bekasi'];
+$prodi = [
+    "SI" => "Sistem Informasi",
+    "TI" => "Teknik Informatika",
+    "BD" => "Bisnis Digital"
+];
+
+$skills = [
+    "HTML" => 10,
+    "CSS" => 10,
+    "JavaScript" => 20,
+    "RWD Bootstrap" => 20,
+    "PHP" => 30,
+    "Pyhon" => 30,
+    "Java" => 50
+];
