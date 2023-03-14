a050b4b21a036793bca574e933fd36e1676962af
abdullahalwafi <abdullahalwafi16@gmail.com>
2023-03-14 14:53:50 +0700

    Tambah cdn boostrap

--- web02/praktikum4/admin/layouts/header.php
+++ web02/praktikum4/admin/layouts/header.php
@@ -10,6 +10,7 @@
     <title>Static Navigation - SB Admin</title>
     <link href="css/styles.css" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 </head>
 
 <body>
